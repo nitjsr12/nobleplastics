1efc23be6831a58bcebfd8da4fed28422a35f2b3
RAVI  kumar <40166356+nitjsr12@users.noreply.github.com>
2023-11-21 19:03:39 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,44 +1,38 @@
 import React from 'react'
 import {Carousel} from 'react-bootstrap';
 import Link from 'next/link';
-import Image from 'next/image';
-import { Navbar, Container, Nav } from 'react-bootstrap';
-import { images } from '@/next.config';
+import Image from 'next/image'
 
 
 function Header() {
   return (
-    <Navbar bg="transparent" variant="dark" expand="lg" className="fixed-top">
-      <Container>
-        <Navbar.Brand href="/"><Image
-          className=" "
-          src="./images/Noble-logo.svg"
-          alt="logo"
-          width={300}
-          height={80}
-        /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarNav" />
-        <Navbar.Collapse id="navbarNav" className="justify-content-end">
-          <Nav className=''>
-            <Nav.Link href="/about-us" >About</Nav.Link>
-            <Nav.Link href="/approach" >Approach</Nav.Link>
-            <Nav.Link href="/work" >Work</Nav.Link>
-            <Nav.Link href="/impact" >Impact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-
-      <style jsx>{`
-        .navbar {
-          background-color: rgba(0, 0, 0, 0.5); /* Set the background color with transparency */
-        }
-        .navbar-brand {
-          color: #fff; /* Set the color of the brand/logo */
-        }
-      
-      `}</style>
-    </Navbar>
-
+    <div >
+     <nav className="navbar fixed-not navbar-expand-lg navbar-light justify-content-center  ">
+    <div className="container"><Link className="nav-link" href="/">
+    <Image src="./images/Noble-logo.svg" className="logo" alt="..." width={400} height={100}/>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsingNavbar3">
+            <span className="navbar-toggler-icon"></span>
+        </button></Link>
+        <div className="navbar-collapse collapse w-100" id="collapsingNavbar3">
+            
+            <ul className="nav navbar-nav ms-auto w-100 justify-content-end">
+                <li className="nav-item">
+                    <Link className="nav-link" href="/about-us">About</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" href="/approach">Approach</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" href="/work">Work</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" href="/impact">Impact</Link>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+</div>
 
   )
 }
