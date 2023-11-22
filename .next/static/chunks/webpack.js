f9075b0ef0ea994d6d8818b2b085b9a52149f34f
Ravi kumar <rk5892962@gmail.com>
2023-11-22 19:04:15 +0530

    final changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "525bc877d23edd42"; }
+/******/ 		__webpack_require__.h = function() { return "2adcdfe581aaf94e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Header.js
+++ components/Header.js
@@ -1,35 +1,22 @@
 import React from 'react'
 import {Carousel} from 'react-bootstrap';
 import Link from 'next/link';
-<<<<<<< HEAD
-import Image from 'next/image'
-=======
 import Image from 'next/image';
 import OffCanvas from './OffCanvas';
->>>>>>> 3d8ad14 (css changes)
+
 
 
 function Header() {
   return (
     <div >
-<<<<<<< HEAD
-     <nav className="navbar fixed-not navbar-expand-lg navbar-light justify-content-center  ">
-    <div className="container"><Link className="nav-link" href="/">
-    <Image src="./images/Noble-logo.svg" className="logo" alt="..." width={400} height={100}/>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsingNavbar3">
-            <span className="navbar-toggler-icon"></span>
-        </button></Link>
-        <div className="navbar-collapse collapse w-100" id="collapsingNavbar3">
-            
-=======
+
      <nav className="navbar fixed-not navbar-expand-lg navbar-light ">
      <div class="container-fluid up-bg">
     <Link href="/" className="navbar-brand">
     <img src="/images/Noble-logo.svg"
     className="img-fluid logo-img"/>
     </Link>
-        <div className="d-none d-lg-block ">
->>>>>>> 3d8ad14 (css changes)
+        <div className="d-none d-lg-block ">)
             <ul className="nav navbar-nav ms-auto w-100 justify-content-end">
                 <li className="nav-item">
                     <Link className="nav-link" href="/about-us">About</Link>
@@ -46,11 +33,9 @@ function Header() {
             </ul>
         </div>
     </div>
-<<<<<<< HEAD
-=======
+
      <OffCanvas/>
->>>>>>> 3d8ad14 (css changes)
-</nav>
+     </nav>
 </div>
 
   )
